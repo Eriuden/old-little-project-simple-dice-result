a357b2213b6627a943ae6b4babc90e1ed7776b93
Antoine Carrette <71089756+Eriuden@users.noreply.github.com>
2023-07-21 19:33:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function diceFace(){
     //Il faut s'y prendre d'une manière un peu détournée
     //avec .length il récupère juste l'index
     //donc: de 0 à 5
-    //donc on lui demande d'afficher l'array selon l'index
+    //donc on lui demande d'afficher la valeur, ici donc un chiffre, qu'a l'index de l'array
+    //c'est pour ça le dice[formule de random] on lui dit "cherche moi une valeur de l'array, désignée par ce calcul"
     //du coup il mettra la valeur qu'il aura pioché au lieu de son index
 
     let results = dice[Math.floor(Math.random()*dice.length)];
